89a16d5405a347be812dca0be8ea04b4494bc057
GlebKolyano <glebkolyano38@gmail.com>
2022-05-18 19:30:31 +0300

    feat: draw main arena of game

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
 import Game from "./Game.js";
+import Loader from "./Loader.js";
+import Sprite from "./Sprite.js";
+
+const scale = 2;
 
 export default async function main() {
   const game = new Game({
-    width: 100,
-    height: 100,
     backgroundColor: 'black',
   });
 
   document.body.append(game.canvas);
   document.body.style.backgroundColor = 'gray';
+
+  const image = await Loader.loadImage('../sets/spritesheet.png');
+  const atlas = await Loader.loadJSON('../sets/atlas.json');
+
+  const maze = new Sprite({
+    image,
+
+    x: 0,
+    y: 0,
+    width: atlas.maze.width * scale,
+    height: atlas.maze.height * scale,
+
+    frame: atlas.maze,
+  });
+
+  game.canvas.width = maze.width;
+  game.canvas.height = maze.height;
+
+  game.store.add(maze);
 }
